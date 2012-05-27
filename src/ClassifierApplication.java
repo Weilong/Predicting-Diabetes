1f2158195b0ae0b1df8fb8b6907b3a9063596f6a
Weilong Ding <tonyaziten@gmail.com>
2012-05-28 01:14:43 +1000

    finished 10-fold validation

--- src/ClassifierApplication.java
+++ src/ClassifierApplication.java
@@ -1,6 +1,165 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ClassifierApplication
 {
-	private String dataSet;
+	private String fileName;
 	private String validation;
-	private String classifier;
+	private String algorithm;
+	private BufferedReader dataSet;
+	private ArrayList<String> attributes;
+	private int numInstance;
+	
+	public ClassifierApplication(String filename) throws IOException
+	{
+		this.fileName = filename;
+		//this.algorithm = algorithm;
+		//this.validation = validation;
+		attributes = new ArrayList<String>();
+		numInstance = 0;
+		readDataSet();
+		preprocessDataSet();
+	}
+	
+	private void readDataSet() throws FileNotFoundException
+	{
+		dataSet = new BufferedReader(new FileReader(fileName));
+	}
+	
+	private void preprocessDataSet() throws IOException
+	{
+		String line = dataSet.readLine();
+		String tmpHeaders[] = line.split(",");
+		for (String  item:tmpHeaders)
+			attributes.add(item);
+		line =dataSet.readLine();
+		
+		while(line!=null)
+		{
+			numInstance++;
+			line = dataSet.readLine();
+		}
+		dataSet.close();
+	}
+	
+	public void run() throws IOException
+	{
+		int folds =10;
+		double accuracySum =0;
+		int instanceNumber;
+		int strat[] = new int[folds];
+		int stratPos[] = new int[folds];
+		String newRecord;
+		NaiveBayes nb = new NaiveBayes();
+		
+		readDataSet();
+		dataSet.readLine();	//skip header line
+		newRecord = dataSet.readLine();
+		
+		while(newRecord!=null)
+		{
+				nb.addTrainingData(newRecord);
+				newRecord = dataSet.readLine();
+		}
+		dataSet.close();
+		nb.training();
+		System.out.println(nb);
+		
+		readDataSet();
+		dataSet.readLine();	//skip header line
+		newRecord = dataSet.readLine();
+		int correctInstances = 0;
+		
+		while(newRecord!=null)
+		{
+					if(nb.testing(newRecord))
+						correctInstances++;
+					newRecord = dataSet.readLine();
+		}
+		dataSet.close();
+		System.out.format("accuracy: %.4f",(double)correctInstances/768);
+		/*for (int i=0;i<folds;i++)
+			strat[i]=numInstance/folds;
+		for (int i=0;i<numInstance%folds;i++)
+			strat[strat.length-1-i]++;
+		for (int i=0;i<folds;i++)
+		{
+			stratPos[i]=1;
+			for (int j=0;j<i;j++)
+			{
+				stratPos[i]+=strat[j];
+			}
+		}
+			
+		for (int i=0;i<folds;i++)
+		{
+			//training
+			readDataSet();
+			dataSet.readLine();	//skip header line
+			newRecord = dataSet.readLine();
+			numInstance++;
+			instanceNumber = 1;
+			
+			while(newRecord!=null)
+			{
+				if (instanceNumber == stratPos[i])
+				{
+					for (int j=0;j<strat[i];j++)
+					{
+						newRecord = dataSet.readLine();
+						instanceNumber++;
+					}
+				}
+				if (newRecord!=null)
+				{
+					nb.addTrainingData(newRecord);
+					newRecord = dataSet.readLine();
+					instanceNumber++;
+				}
+			}
+			dataSet.close();
+			nb.training();
+			//System.out.println(nb);
+			
+			//testing
+			readDataSet();
+			dataSet.readLine();	//skip header line
+			newRecord = dataSet.readLine();
+			numInstance++;
+			int correctInstances = 0;
+			instanceNumber=1;
+			
+			while(newRecord!=null)
+			{
+				if (instanceNumber == stratPos[i])
+				{
+					for (int j=0;j<strat[i];j++)
+					{
+						if(nb.testing(newRecord))
+							correctInstances++;
+						newRecord = dataSet.readLine();
+						instanceNumber++;
+					}
+					break;
+				}
+				newRecord = dataSet.readLine();
+				instanceNumber++;
+			}
+			dataSet.close();
+			System.out.format("%d folds accuracy: %.4f",(i+1),(double)correctInstances/(double)strat[i]);
+			System.out.println();
+			accuracySum+=(double)correctInstances/(double)strat[i];
+			nb.reset();
+		}
+		System.out.format("Average accuracy over the %d runs: %.4f",folds,accuracySum/folds);*/
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		ClassifierApplication classifier = new ClassifierApplication(args[0]);
+		classifier.run();
+	}
 }
--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -1,76 +1,68 @@
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Scanner;
 
 public class NaiveBayes
 {
-	//private String headers[];
-	private ArrayList<String> headers;
+	//private ArrayList<String> headers;
 	private HashMap<String,ArrayList<Double>> meanMap;
 	private HashMap<String,ArrayList<Double>> SDMap;
+	private HashMap<String,Double> classPriors;
 	private HashMap<String,Integer> numClass;
 	
-	public NaiveBayes(String filename)
+	public NaiveBayes() throws FileNotFoundException
 	{
-		headers = new ArrayList<String>();
+		//headers = new ArrayList<String>();
 		meanMap = new HashMap<String,ArrayList<Double>>();
 		SDMap = new HashMap<String,ArrayList<Double>>();
+		classPriors = new HashMap<String,Double>();
 		numClass = new HashMap<String,Integer>();
-		
-		try {
-			processFile(filename);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
 	}
 	
-	private void processFile(String filename) throws FileNotFoundException
+	public void addTrainingData(String record)
 	{
-		Scanner s = new Scanner(new File(filename));
-		String line;
 		String className;
-		String tmp[];
-
-		line = s.nextLine();
-		String tmpHeaders[] = line.split(",");
-		for (String  item:tmpHeaders)
-			headers.add(item);
-		//test
-		/*for (int i=0;i<headers.size();i++)
-			System.out.print(headers.get(i)+",");
-		System.out.println();*/
-		while (s.hasNextLine())
+		String values[];
+		
+		values = record.split(",");
+		className = values[values.length-1];
+		
+		if(!meanMap.containsKey(className)&&!SDMap.containsKey(className))
 		{
-			line = s.nextLine();
-			tmp = line.split(",");
-			//test
-			/*for (int i=0;i<tmp.length;i++)
-				System.out.print(tmp[i]+",");
-			System.out.println();*/
-			className = tmp[tmp.length-1];
-			//test
-			//System.out.println(className);
-			if(!meanMap.containsKey(className)&&!SDMap.containsKey(className))
+			meanMap.put(className, new ArrayList<Double>());
+			SDMap.put(className, new ArrayList<Double>());
+			classPriors.put(className, (double)0);
+			numClass.put(className, 0);
+			
+			for (int i=0;i<values.length-1;i++)
 			{
-				meanMap.put(className, new ArrayList<Double>());
-				SDMap.put(className, new ArrayList<Double>());
-				numClass.put(className, 0);
-				
-				for (int i=0;i<tmp.length-1;i++)
-				{
-					meanMap.get(className).add((double)0);
-					SDMap.get(className).add((double)0);
-				}
+				meanMap.get(className).add((double)0);
+				SDMap.get(className).add((double)0);
 			}
-			for (int i=0;i<tmp.length-1;i++)
-			{
-				meanMap.get(className).set(i, meanMap.get(className).get(i)+Double.parseDouble(tmp[i]));
-				SDMap.get(className).set(i, SDMap.get(className).get(i)+Math.pow(Double.parseDouble(tmp[i]), 2));
-			}
-			numClass.put(className,numClass.get(className)+1);
+		}
+		
+		for (int i=0;i<values.length-1;i++)
+		{
+			
+			meanMap.get(className).set(i, meanMap.get(className).get(i)+Double.parseDouble(values[i]));
+			SDMap.get(className).set(i, SDMap.get(className).get(i)+Math.pow(Double.parseDouble(values[i]), 2));
+		}
+		numClass.put(className,numClass.get(className)+1);
+	}
+	
+	public void training() throws IOException
+	{
+		String className;
+		int numInstance = 0;
+		for (Iterator<String> it = numClass.keySet().iterator();it.hasNext();)
+			numInstance+=numClass.get(it.next());
+		
+		for (Iterator<String> it = numClass.keySet().iterator();it.hasNext();)
+		{
+			className = it.next();
+			classPriors.put(className, numClass.get(className)/(double)numInstance);
 		}
 		
 		for (Iterator<String> it = meanMap.keySet().iterator();it.hasNext();)
@@ -78,7 +70,9 @@ public class NaiveBayes
 			className = it.next();
 			ArrayList<Double> list = meanMap.get(className);
 			for (int i=0;i<list.size();i++)
-				list.set(i, list.get(i)/numClass.get(className));
+			{
+				list.set(i, list.get(i)/numClass.get(className));			
+			}
 		}
 				
 		for (Iterator<String> it = SDMap.keySet().iterator();it.hasNext();)
@@ -92,22 +86,68 @@ public class NaiveBayes
 			}
 		}
 	}
-	//TODO getMean
-	public double getMean(String className,String attribute)
+	
+	public boolean testing(String record)
 	{
-		return meanMap.get(className).get(headers.indexOf(attribute));	
+		String correctClass = null;
+		String testClass = null;
+		String values[];
+		values = record.split(",");
+		correctClass = values[values.length-1];
+		double likelihood,maxProb=0;
+		Iterator<String> meanIt = meanMap.keySet().iterator();
+		Iterator<String> sdIt = SDMap.keySet().iterator();
+
+		while(meanIt.hasNext()&&sdIt.hasNext())
+		{
+			String className = meanIt.next();
+			likelihood=1;
+			ArrayList<Double> meanList = meanMap.get(className);
+			ArrayList<Double> sdList = SDMap.get(className);	
+			
+			for (int i=0;i<meanList.size();i++)
+				likelihood*= PDF(Double.parseDouble(values[i]),meanList.get(i),sdList.get(i));
+			likelihood*=classPriors.get(className);
+			System.out.println(className);
+			System.out.println(likelihood);
+			if (likelihood>maxProb)
+			{
+				maxProb = likelihood;
+				testClass = className;
+			}
+		}
+		return testClass.equals(correctClass);
 	}
-	//TODO getSD
-	public double getSD(String className,String attribute)
+	
+	public void reset()
 	{
-		return meanMap.get(className).get(headers.indexOf(attribute));
+		meanMap.clear();
+		SDMap.clear();
+		numClass.clear();
+		classPriors.clear();
 	}
-	//TODO toString
+	
+	private double PDF(double x,double mean,double sd)
+	{
+		//System.out.println("x: "+ x +" mean: "+mean+" sd: "+sd);
+		//System.out.println(1/(sd*Math.sqrt(2*Math.PI))*Math.exp(-Math.pow(x-mean, 2)/2*Math.pow(sd, 2)));
+		return 1/(sd*Math.sqrt(2*Math.PI))*Math.exp(-Math.pow(x-mean, 2)/2*Math.pow(sd, 2));
+	}
+	
+	public ArrayList<Double> getMeans(String className)
+	{
+		return meanMap.get(className);	
+	}
+	
+	public ArrayList<Double> getSD(String className,String attribute)
+	{
+		return meanMap.get(className);
+	}
+	
 	public String toString()
 	{
 		System.out.println("Mean table:");
-		for (String item:headers)
-			System.out.print(item+" ");
+		
 		System.out.println();
 		for (Iterator<String> it = meanMap.keySet().iterator();it.hasNext();)
 		{
@@ -121,8 +161,7 @@ public class NaiveBayes
 		System.out.println();
 		
 		System.out.println("Standard Deviation table:");
-		for (String item:headers)
-			System.out.print(item+" ");
+		
 		System.out.println();
 		for (Iterator<String> it = SDMap.keySet().iterator();it.hasNext();)
 		{
@@ -135,11 +174,5 @@ public class NaiveBayes
 		}
 		
 		return "";
-	}
-	
-	public static void main(String[] args)
-	{
-		NaiveBayes classifier = new NaiveBayes(args[0]);
-		System.out.println(classifier);
 	}
 }
