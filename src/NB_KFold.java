2102854472ee23de6d0e874c072a11215f1f811d
Weilong Ding <tonyaziten@gmail.com>
2012-05-28 15:19:05 +1000

    finished code documentation

--- src/NB_KFold.java
+++ src/NB_KFold.java
@@ -4,36 +4,73 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
-
+/**
+ * COMP3308 Introduction to AI Assignment2
+ * NB_KFold.java
+ * Purpose: the main class of the program. receive the command from user
+ * and do the Naive Bayes algorithm on the given dataset with k-fold val-
+ * idation. The program will print out the accuracry for each fold and an
+ * average accuracy will be printed out at the end. The number of folds
+ * must be greater than 1, or a warning will be given.
+ *
+ * @author Weilong Ding/Ruijun GU
+ * @version 1.0 27/05/2012
+ */
 public class NB_KFold
 {
 	private String fileName;
-	private BufferedReader dataSet;
-	private ArrayList<String> attributes;
-	private int numInstance;
+	private BufferedReader dataSet;	
+	private ArrayList<String> attributes;	//store the names of the attributes of the dataset
+	private int numInstance;	//the number of total instances
+	private int folds;	//the number of folds Naive Nayes runs
 	
-	public NB_KFold(String filename) throws IOException
+	/**
+	 * This is the constructor of NB_KFold class which will read
+	 * a file and the number of folds to initialize and preprocess
+	 * the dataset
+	 * @param filename the url of the input dataset
+	 * @param folds the number of folds user what to do
+	 * @throws IOException
+	 */
+	public NB_KFold(String filename,int folds) throws IOException
 	{
 		this.fileName = filename;
+		this.folds = folds;
 		attributes = new ArrayList<String>();
 		numInstance = 0;
 		readDataSet();
 		preprocessDataSet();
 	}
 	
+	/**
+	 * Method readDataSet initialize the BufferedReader of the
+	 * input dataset
+	 * @throws FileNotFoundException
+	 */
 	private void readDataSet() throws FileNotFoundException
 	{
 		dataSet = new BufferedReader(new FileReader(fileName));
 	}
 	
+	/**
+	 * Method preprocessDataSet read and store the names of all
+	 * the attributes and calculate the number of instances in 
+	 * the dataset
+	 * @throws IOException
+	 */
 	private void preprocessDataSet() throws IOException
 	{
+		/*
+		 * read the first line of dataset, which is the 
+		 * header line and store the names of the attri-
+		 * butes in attributes list.
+		 */
 		String line = dataSet.readLine();
 		String tmpHeaders[] = line.split(",");
 		for (String  item:tmpHeaders)
 			attributes.add(item);
 		line =dataSet.readLine();
-		
+		//calculate the number of instances in the dataset
 		while(line!=null)
 		{
 			numInstance++;
@@ -41,21 +78,49 @@ public class NB_KFold
 		}
 		dataSet.close();
 	}
-	
-	public void run(int folds) throws IOException
+	/**
+	 * Method run executes the most function of NB_KFold class.
+	 * It stratifies the dataset according to the the number of
+	 * folds from user's input and do Naive Bayes algorithm. The
+	 * accuracy of each fold will be printed out in percentage and
+	 * an average accuracy will be printed out at the end.
+	 * @throws IOException
+	 */
+	public void run() throws IOException
 	{
-		double accuracySum =0;
+		//if the number of folds from input is not greater than 1
+		//show warning and exit program
+		if (folds<2)
+		{
+			System.out.println("Warning! Number of folds must be greater than 1!");
+			return;
+		}
+		double accuracySum =0;	//the sum of accuracies of all folds
 		int instanceNumber;
-		int strat[] = new int[folds];
-		int stratPos[] = new int[folds];
-		String newRecord;
+		int strat[] = new int[folds];	//stores how many instances in each stratification
+		int stratPos[] = new int[folds];	//stores the position where stratifiction begins
+		String newInstance;
 		NumberFormat nf = NumberFormat.getPercentInstance();
-		NaiveBayes nb = new NaiveBayes(attributes);
-		
+		NaiveBayes nb = new NaiveBayes(attributes);	//create an instance of NaiveBayes class
+		/*
+		 * since stratification needs to be done as evenly as possible
+		 * first we divide numInstace by folds and store the result in 
+		 * strat list. so far each item of strat is the same.Secondly we get
+		 * the reminder and allocate it evenly to the existed stratification.
+		 * eg in this program the dataset has 768 instances. firstly we divide 
+		 * 768 by 10 and we get 76 and allocate the reminder 8 to the existed 
+		 * stratification so that we get 76,76,77,77,77,77,77,77,77,77.
+		 */
 		for (int i=0;i<folds;i++)
 			strat[i]=numInstance/folds;
 		for (int i=0;i<numInstance%folds;i++)
 			strat[strat.length-1-i]++;
+		/*
+		 * calculate the postion where the stratification starts
+		 * if we know the position, we can skip instances from that position
+		 * for certain instances since the skipped instances are reserved for
+		 * testing.
+		 */
 		for (int i=0;i<folds;i++)
 		{
 			stratPos[i]=1;
@@ -64,62 +129,74 @@ public class NB_KFold
 				stratPos[i]+=strat[j];
 			}
 		}
-			
+		//run Naive Bayes algorithm with k-fold validation
 		for (int i=0;i<folds;i++)
 		{
-			//training
+			//training starts
 			readDataSet();
 			dataSet.readLine();	//skip header line
-			newRecord = dataSet.readLine();
+			newInstance = dataSet.readLine();
 			numInstance++;
-			instanceNumber = 1;
-			
-			while(newRecord!=null)
+			instanceNumber = 1;	//track which instance has been reached
+			//read each line until file reaches end
+			while(newInstance!=null)
 			{
+				/*
+				 * if the current instance is the one where stratification
+				 * should start, skip certain number of instances (which is 
+				 * for testing, the number of instances to skip is stored in 
+				 * strat list) and continue to read the data
+				 */
 				if (instanceNumber == stratPos[i])
 				{
 					for (int j=0;j<strat[i];j++)
 					{
-						newRecord = dataSet.readLine();
+						newInstance = dataSet.readLine();
 						instanceNumber++;
 					}
 				}
-				if (newRecord!=null)
+				if (newInstance!=null)
 				{
-					nb.addTrainingData(newRecord);
-					newRecord = dataSet.readLine();
+					nb.addTrainingData(newInstance);
+					newInstance = dataSet.readLine();
 					instanceNumber++;
 				}
 			}
 			dataSet.close();
 			nb.training();
 			
-			//testing
+			//testing starts
 			readDataSet();
 			dataSet.readLine();	//skip header line
-			newRecord = dataSet.readLine();
+			newInstance = dataSet.readLine();
 			numInstance++;
-			int correctInstances = 0;
+			int correctInstances = 0;	//track the number of correct classified instances
 			instanceNumber=1;
 			
-			while(newRecord!=null)
+			while(newInstance!=null)
 			{
+				/*
+				 * if the current instance is where the stratification
+				 * starts, test the instance until the testing portion 
+				 * of the dataset finishes, otherwise skip the instance. 
+				 */
+				
 				if (instanceNumber == stratPos[i])
 				{
 					for (int j=0;j<strat[i];j++)
 					{
-						if(nb.testing(newRecord))
-							correctInstances++;
-						newRecord = dataSet.readLine();
+						if(nb.testing(newInstance))
+							correctInstances++;	//if the instance is correctly classified,increment correctInstances
+						newInstance = dataSet.readLine();
 						instanceNumber++;
 					}
-					break;
+					break;	//after finish the testing data portion, stop reading  dataset since no need
 				}
-				newRecord = dataSet.readLine();
+				newInstance = dataSet.readLine();
 				instanceNumber++;
 			}
 			dataSet.close();
-			
+			//calculate the accuracy for each fold and print it out
 			nf.setMinimumFractionDigits(4);
 			System.out.println((i+1) + ((i+1)==1?" fold":" folds")+" accuracy: "+ nf.format((double)correctInstances/(double)strat[i]));
 			System.out.println();
@@ -130,8 +207,8 @@ public class NB_KFold
 	}
 	
 	public static void main(String[] args) throws IOException
-	{System.out.println(args[0]);
-		NB_KFold classifier = new NB_KFold(args[0]);
-		classifier.run(Integer.parseInt(args[1]));
+	{
+		NB_KFold classifier = new NB_KFold(args[0],Integer.parseInt(args[1]));
+		classifier.run();
 	}
 }
--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -3,68 +3,93 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-
+/**
+ * COMP3308 Introduction to AI Assignment2
+ * NaiveBayes.java
+ * Purpose: This NaiveBayes class receives given dataset and calculate
+ * the mean and standard deviation for each attributes of all the classes
+ * respectively during the training and receiveds dataset for testing to do
+ * test using probability density function during the testing.
+ * @author Weilong Ding/Ruijun GU
+ * @version 1.0 27/05/2012
+ */
 public class NaiveBayes
 {
-	private ArrayList<String> attributes;
-	private HashMap<String,ArrayList<Double>> meanMap;
-	private HashMap<String,ArrayList<Double>> SDMap;
-	private HashMap<String,Double> classPriors;
-	private HashMap<String,Integer> numClass;
+	private ArrayList<String> attributes;	//store names of the attributes
+	private HashMap<String,ArrayList<Double>> meanMap;	//store means of attributes for each class
+	private HashMap<String,ArrayList<Double>> sdMap;	//store std.dev of attributes for each class
+	private HashMap<String,Double> classPriors;	//store Priors of each class
+	private HashMap<String,Integer> numClass;	//store the number of each class
 	
+	/**
+	 * This is the constructor of NaiveBayes class which will receive
+	 * the list of attribute names as parameter and initialize all necessary
+	 * data structure
+	 * @param attritubes the list of attribute names
+	 * @throws FileNotFoundException
+	 */
 	public NaiveBayes(ArrayList<String> attributes) throws FileNotFoundException
 	{
 		this.attributes = attributes;
 		meanMap = new HashMap<String,ArrayList<Double>>();
-		SDMap = new HashMap<String,ArrayList<Double>>();
+		sdMap = new HashMap<String,ArrayList<Double>>();
 		classPriors = new HashMap<String,Double>();
 		numClass = new HashMap<String,Integer>();
 	}
 	
-	public void addTrainingData(String record)
+	/**
+	 * Method addTrainingData receives an instance and process it
+	 * to store the crude data in meanMap and SDMap for later use
+	 * @param instance
+	 */
+	public void addTrainingData(String instance)
 	{
 		String className;
 		String values[];
 		
-		values = record.split(",");
+		values = instance.split(",");
 		className = values[values.length-1];
-		
-		if(!meanMap.containsKey(className)&&!SDMap.containsKey(className))
+		//if a class has  not been stored , store it 
+		if(!meanMap.containsKey(className)&&!sdMap.containsKey(className))
 		{
 			meanMap.put(className, new ArrayList<Double>());
-			SDMap.put(className, new ArrayList<Double>());
+			sdMap.put(className, new ArrayList<Double>());
 			classPriors.put(className, (double)0);
 			numClass.put(className, 0);
 			
 			for (int i=0;i<values.length-1;i++)
 			{
 				meanMap.get(className).add((double)0);
-				SDMap.get(className).add((double)0);
+				sdMap.get(className).add((double)0);
 			}
 		}
-		
+		//store the crude data of all the attributes of each class
 		for (int i=0;i<values.length-1;i++)
 		{
-			
 			meanMap.get(className).set(i, meanMap.get(className).get(i)+Double.parseDouble(values[i]));
-			SDMap.get(className).set(i, SDMap.get(className).get(i)+Math.pow(Double.parseDouble(values[i]), 2));
+			sdMap.get(className).set(i, sdMap.get(className).get(i)+Math.pow(Double.parseDouble(values[i]), 2));
 		}
 		numClass.put(className,numClass.get(className)+1);
 	}
 	
+	/**
+	 * Method training uses the crude data in meanMap and sdMap to calculate
+	 * the mean and standard deviation for all attributes of each class.
+	 * @throws IOException
+	 */
 	public void training() throws IOException
 	{
 		String className;
 		int numInstance = 0;
 		for (Iterator<String> it = numClass.keySet().iterator();it.hasNext();)
 			numInstance+=numClass.get(it.next());
-		
+		//calculate the priors of each class
 		for (Iterator<String> it = numClass.keySet().iterator();it.hasNext();)
 		{
 			className = it.next();
 			classPriors.put(className, numClass.get(className)/(double)numInstance);
 		}
-		
+		//calculate means of all attributes for each class
 		for (Iterator<String> it = meanMap.keySet().iterator();it.hasNext();)
 		{
 			className = it.next();
@@ -74,11 +99,11 @@ public class NaiveBayes
 				list.set(i, list.get(i)/numClass.get(className));			
 			}
 		}
-				
-		for (Iterator<String> it = SDMap.keySet().iterator();it.hasNext();)
+		//calculate std.dev of all attributes for each class		
+		for (Iterator<String> it = sdMap.keySet().iterator();it.hasNext();)
 		{
 			className = it.next();
-			ArrayList<Double> list = SDMap.get(className);
+			ArrayList<Double> list = sdMap.get(className);
 			for (int i=0;i<list.size();i++)
 			{
 				double tmpSD = Math.sqrt(list.get(i)/numClass.get(className)-Math.pow(meanMap.get(className).get(i),2));
@@ -87,28 +112,39 @@ public class NaiveBayes
 		}
 	}
 	
-	public boolean testing(String record)
+	/**
+	 * Method testing receives an instance and classify it using
+	 * probability density function where the mean and std.dev are
+	 * from training. return the testing result
+	 * @param instance the instance for classifying
+	 * @return true if classification is correct otherwise false.
+	 */
+	public boolean testing(String instance)
 	{
 		String correctClass = null;
 		String testClass = null;
 		String values[];
-		values = record.split(",");
+		values = instance.split(",");
 		correctClass = values[values.length-1];
 		double likelihood,maxProb=0;
 		Iterator<String> meanIt = meanMap.keySet().iterator();
-		Iterator<String> sdIt = SDMap.keySet().iterator();
-
+		Iterator<String> sdIt = sdMap.keySet().iterator();
+		/*
+		 * calculate the probability of each class and compare
+		 * choose the class with greater probabilty as the classified
+		 * result
+		 */
 		while(meanIt.hasNext()&&sdIt.hasNext())
 		{
 			String className = meanIt.next();
 			likelihood=1;
 			ArrayList<Double> meanList = meanMap.get(className);
-			ArrayList<Double> sdList = SDMap.get(className);	
-			
+			ArrayList<Double> sdList = sdMap.get(className);	
+			//calculate the likelihood of each attribute and multiply them together
 			for (int i=0;i<meanList.size();i++)
 				likelihood*= pdf(Double.parseDouble(values[i]),meanList.get(i),sdList.get(i));
-			likelihood*=classPriors.get(className);//System.out.println();
-			
+			likelihood*=classPriors.get(className);	//multiply with class prior
+			//get the maximum probability and the class with this probabilty
 			if (likelihood>maxProb)
 			{
 				maxProb = likelihood;
@@ -118,19 +154,34 @@ public class NaiveBayes
 		return testClass.equals(correctClass);
 	}
 	
+	/**
+	 * reset the NaiveBayes instance
+	 */
 	public void reset()
 	{
 		meanMap.clear();
-		SDMap.clear();
+		sdMap.clear();
 		numClass.clear();
 		classPriors.clear();
 	}
 	
+	/**
+	 * Normal Probability Density Function
+	 * @param x
+	 * @param mean mean(or expected value)
+	 * @param sd standard deviation
+	 * @return
+	 */
 	private double pdf(double x,double mean,double sd)
 	{
 		return 1/(sd*Math.sqrt(2*Math.PI))*Math.exp(-Math.pow(x-mean, 2)/(2*Math.pow(sd, 2)));
 	}
 	
+	/**
+	 * @override
+	 * Override toString method.
+	 * Print out the training result of Naive Bayes in certain format
+	 */
 	public String toString()
 	{
 		System.out.println("Mean table:");
@@ -154,10 +205,10 @@ public class NaiveBayes
 		for (String attribute:attributes)
 			System.out.printf("%17s",attribute);
 		System.out.println();
-		for (Iterator<String> it = SDMap.keySet().iterator();it.hasNext();)
+		for (Iterator<String> it = sdMap.keySet().iterator();it.hasNext();)
 		{
 			String className = it.next();
-			ArrayList<Double> list = SDMap.get(className);
+			ArrayList<Double> list = sdMap.get(className);
 			for (double value:list)
 				System.out.printf("%17.4f",value);
 			System.out.printf("%17s",className);
